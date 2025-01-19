a18704cbf03757e04a7da1b9fe5d4573d9dcc942
minervatbd <windmillerg@gmail.com>
2025-01-18 19:41:00 -0500

    send_message will be formatted if given a user_target

--- utils.py
+++ utils.py
@@ -1,10 +1,32 @@
 import datetime
+import discord
+
+TERMINATE = False
 
 """ internal console log messages """
 def logMsg(string):
     print("[{}] {}".format(datetime.datetime.now(), string))
 
     return string
+
+async def send_message(channel, user_target = None, text = None, embed = None, delete_after = None):
+    try:
+        if text is not None:
+            
+            if user_target is not None:
+                  text = formatMessage(user_target, text)
+                  
+            return await channel.send(content=text, delete_after=delete_after)
+        if embed is not None:
+            return await channel.send(embed=embed)
+    except discord.errors.Forbidden:
+        logMsg('Could not message user: {}\n{}'.format(channel, text))
+        raise
+    except:
+        logMsg('Failed to send message to channel: {}\n{}'.format(channel, text))
+
+def formatMessage(user_target, message):
+    return "*{}*: {}".format(user_target.display_name, message).replace("@", "\{at\}")
 
 """ read a file named fname and return its contents as a string """
 def getValueFromFileContents(fname):
